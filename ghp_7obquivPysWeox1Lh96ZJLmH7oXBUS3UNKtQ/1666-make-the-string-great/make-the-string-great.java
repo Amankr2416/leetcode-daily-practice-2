ef80ac7d2b49b90b088e4c4861a56125daff68e0
Amankr2416 <amankumarmishra193@gmail.com>
2026-04-09 20:26:29 +0530

    Time: 2 ms (76.14%) | Memory: 43.2 MB (95.24%) - LeetSync

---
+++ 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/1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder stack = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            int len = stack.length();
+
+            if (len > 0 && Math.abs(stack.charAt(len - 1) - c) == 32) {
+                stack.deleteCharAt(len - 1); // remove bad pair
+            } else {
+                stack.append(c);
+            }
+        }
+
+        return stack.toString();
+    }
+}
