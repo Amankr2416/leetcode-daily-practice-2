f8ef8e1c81d1d4e835df08dfc5b0b5e26729c5cf
Amankr2416 <amankumarmishra193@gmail.com>
2026-04-18 21:23:05 +0530

    Time: 2 ms (100.00%) | Memory: 102.2 MB (42.09%) - LeetSync

---
+++ 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/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        long leftSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSum += nums[i];
+
+            if (leftSum >= total - leftSum) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
