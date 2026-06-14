1e819e2275cbf0d8b61f78c55a1eeb9111784aea
Amankr2416 <amankumarmishra193@gmail.com>
2026-06-14 09:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.40%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -5,7 +5,8 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[k++] = nums[i];
+                nums[k] = nums[i];
+                k++;
             }
         }
 
